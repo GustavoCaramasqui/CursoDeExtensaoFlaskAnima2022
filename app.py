7c4704d915f87db0f52d3851f9fb3ed9630badfc
Gustavo Caramasqui <gucaramasqui@gmail.com>
2022-08-11 21:23:32 -0300

    Usando o Jinja2 arquivo alo.html

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+os.system('cls')
+
+from flask import Flask, render_template
+app = Flask("projeto")
+
+@app.route ("/")
+def ola_mundo ():
+    return render_template("alo.html"), 200
+
+app.run()
